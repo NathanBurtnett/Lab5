5647ef2060a93d53bc8e07a86e9cf0682db8e179
NathanBurtnett <nathanburtnett@gmail.com>
2022-11-17 09:41:14 -0800

    TCOISR INIT

--- Code/Sources/main.asm
+++ Code/Sources/main.asm
@@ -47,14 +47,12 @@
 ;\------------------------------------------------------------------------------------/
 ; Constant values can be equated here
 
-
 TFLG1         EQU   $004E
 TC0           EQU   $0050
 C0F           EQU   %00000001          ; timer channel 0 output compare bit
 PORTT         EQU   $0240              ; PORTT pin 8 to be used for interrupt timing
 LOWER_LIM     EQU   -625               ; number for max reverse duty cycle
 UPPER_LIM     EQU   625                ; number for max forward duty cycle
-
 
 ;/------------------------------------------------------------------------------------\
 ;| Variables in RAM                                                                   |
@@ -69,10 +67,19 @@ CL:           DS.B  1                  ; Boolean for closed-loop active
 V_ref:        DS.W  1                  ; reference velocity
 Theta_OLD:    DS.W  1                  ; previous encoder reading
 KP:           DS.W  1                  ; proportional gain
+KPRES:        DS.W  1                  ; proportional gain result
 KI:           DS.W  1                  ; integral gain
+KIRES:        DS.W  1                  ; intergral gain result
+
+A             DS.W  1                  ; pi controller output
+ASTAR         DS.W  1                  ; saturation checked pi controller output
+
+ERROR:        DS.W  1                  ; the current error of the system
+ESUM:         DS.W  1                  ; reiman sum of errors
+
+EFF:          DS.W  1                  ; current effort of motor
 
 UPDATE_FLG1   DS.B  1                  ; Boolean for display update for line one
-
 
 
 ;/------------------------------------------------------------------------------------\
@@ -82,14 +89,150 @@ UPDATE_FLG1   DS.B  1                  ; Boolean for display update for line one
 
 MyCode:       SECTION
 main:   
-        
-spin:   bra   spin                     ; endless horizontal loop
+;CLEAR VARIABLES 
+  clr LVREF_BUF
+  clr LVACT_BUF
+  clr LVACT_BUF
+  clr 
 
+  ;SETUP INSTRUCTIONS
+  jsr STARTUP_ENCODER   ;initialize encoder
+  jsr READ_ENCODER      ;returns encoder count in D
+  std ENCODER_COUNT     ;store the count in a 16-bit variable in RAM
+
+  jsr STARTUP_PWM       ;initialize PWM module
+  jsr STARTUP_MOTOR     ;initialize motor in disabled state
+
+  jsr ENABLE_MOTOR      ;enable motor operation
+
+  jsr INITLCD
+  jsr LCDTEMPLATE       ;initializes the LCD
+  bgnd
+  
+
+  TOP:
+
+    bra TOP               ;go back to TOP and loop through endlessly
+
+    jsr FREDENTRY         ;
+    spin: bra TOP
+
+
+
+;||||||||||||||||||||||||||||||||||ISR|||||||||||||||||||||||||||||||||||||||||||||||||
+TC0ISR:
+  bset PORTT, $80           ;turn on PORTT pin 8 to begin ISR timing
+
+  inc UPDATE_COUNT          ;unless UPDATE_COUNT = 0, skip saving
+  bne measurements          ; display variables
+  movw V_act, V_act_DISP    ;take a snapshot of variables to enable
+  movw ERR, ERR_DSIP        ; consistent display
+  movw EFF, EFF_DISP
+  movb #$01, UPDATE_FLG1    ;set UPDATEFLG1 when appropriate
+
+  measurements:
+    ;read encoder value
+    jsr   READ_ENCODER          ;read encoder position
+    std   Theta_NEW             ;store it
+    ;compute 2-point difference to get speed
+    subd  Theta_OLD             ;compute displacement since last reading
+    std   V_act                 ;store displacement as actual speed
+    movw  Theta_NEW, Theta_OLD  ;move current raeading to previous reading
+
+    ;calculate Vact
+
+    ;compute the error
+    ldd   V_ref
+    subd  V_act
+    std   ERROR
+    ldy   ESUM
+    jsr   DSATADD
+    std   ESUM
+    ;compute Ki
+    ldy KI
+    emul 
+    ldx #$0400
+    idiv 
+    stx KIRES
+    ;compute Kp
+    ldd ERROR
+    ldy KP
+    emul 
+    ldx #$0400
+    idiv 
+    stx KPRES
+    ;add Kp and Ki
+    ldd KPRES
+    ldy KIRES
+    jsr DSATADD
+    std A
+    ;satcheck
+    jsr SATCHECK
+    std ASTAR
+    ;calculate effort
+    ldy #$0271
+    emul
+    ldx #$0064
+    idiv
+    stx EFF
+    ;set motor speed
+    ldd ASTAR
+    jsr UPDATE_MOTOR
+    
+    
+    
+  ;CALCULATE VACT
+    ;VACT = (THETANEW-THETAOLD)/(1BTI)
+  ;CALCULATE ERROR (VREF-VACT)
+  ;CALCULATE CONTROLER EFFORT
+  ;CALCULATE PWM INPUT
 
 ;/------------------------------------------------------------------------------------\
 ;| Subroutines                                                                        |
 ;\------------------------------------------------------------------------------------/
-; General purpose subroutines go here
+SATCHECK:
+    pshx
+    pshc
+    cpd #$0000
+    bmi NEGATIVE
+    bgt POSITIVE
+    bra SATCHECK_exit
+    POSITIVE:
+        cpd #$0271
+        ble SATCHECK_exit
+        ldd #$0271
+        bra SATCHECK_exit
+    NEGATIVE:
+        cpd #$FD8F
+        bge SATCHECK_exit
+        ldd #$FD8F
+    SATCHECK_exit:
+        pulc 
+        pulx
+        rts
+
+DSATADD:
+    pshx                ;pushes x to stack
+    pshc                ;pushes c to stack
+    sty TEMP            ;stores acc y value in TEMP
+    addd TEMP           ;adds acc D to TEMP
+    bvs DSATADD_OF    ;if Overflow is set then branch, otherwise just fall to exit
+  
+    DSATADD_exit:
+        pulc                ;puls c from stack
+        pulx                ;puls x from stack
+        rts                 ;end subroutine
+
+    DSATADD_OF:         ;tests overflow
+        cpy #$0000          ;compares TEMP with hex 0
+        bmi DSATADD_OFN          ;branches if value is negative
+        ldd #$7FFF          ;loads acc D with positive overflow
+        bra DSATADD_exit    ;goes to end subroutine
+
+    DSATADD_OFN:        ;sets negative overflow
+        ldd #$8000          ;loads acc D with negative overflow
+        bra DSATADD_exit    ;goes to end subroutine  
+
 
 
 ;/------------------------------------------------------------------------------------\
